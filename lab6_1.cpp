d71b871b2b0480c83e1a50eb4ac0c3d6a3d0a41f
keihanikakauLOL <arm.nantana@gmail.com>
2025-12-09 15:09:13 +0700

    Filled out lab6_1

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,24 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+
+    int num = 1;
+    int odd = 0;
+    int even = 0;
+
+    while (num != 0) {
+        cout << "Enter an integer: ";
+        cin >> num;
+
+        if (num % 2 == 0) {
+            even++;
+        }
+        else {
+            odd++;
+        }
+    }
+
+    cout << "#Even numbers = " << even;
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
